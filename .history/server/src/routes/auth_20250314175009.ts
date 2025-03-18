30058741076effef4fcf689ea5a3d294f79e31f8
kagisho96 <kagishopitsi@gmail.com>
2025-03-18 15:49:56 +0200

    fix: auth

---
+++ .history/server/src/routes/auth_20250314175009.ts
@@ -0,0 +1,16 @@
+import express, { RequestHandler } from "express";
+import { getCurrentUser, login, register } from "../controllers/auth";
+import { authenticate } from "../middleware/auth";
+
+const router = express.Router();
+
+// Register route
+router.post('/register', register as RequestHandler);
+
+// Login route
+router.post('/login', login as RequestHandler);
+
+// Get current user route (protected)
+router.get('/me', authenticate as RequestHandler, getCurrentUser as RequestHandler);
+
+export default router; 
